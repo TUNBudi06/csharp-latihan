81e9a586190f7891221ebaae09728f68808aa2ed
TUNBudi06 <PESERTA LKS@DESKTOP-6S50NM7>
2024-01-26 10:22:27 +0700

    refactor the logic

--- bromo/Register.cs
+++ bromo/Register.cs
@@ -69,6 +69,21 @@ namespace bromo
 
         private void textBox_noTelp_KeyPress(object sender, KeyPressEventArgs e)
         {
+            if (!Char.IsDigit(e.KeyChar) && (e.KeyChar != (char)(Keys.Back)))
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                // is a digit or backspace - ignore digits if length is alreay 10 - allow backspace
+                if (Char.IsDigit(e.KeyChar))
+                {
+                    if (textBox_noTelp.Text.Length > 14)
+                    {
+                        e.Handled = true;
+                    }
+                }
+            }
             if (e.KeyChar == (char)Keys.Enter)
             {
                 textBox_password.Focus();
@@ -90,7 +105,8 @@ namespace bromo
             try
             {
                 //logic of username
-                if(!string.IsNullOrEmpty(textBox_username.Text)) {
+                if (!string.IsNullOrEmpty(textBox_username.Text))
+                {
                     string getUsername = "select * from Akun where Username = '" + textBox_username.Text + "'";
                     SqlDataAdapter data_user = new SqlDataAdapter(getUsername, conn);
 
@@ -100,30 +116,69 @@ namespace bromo
                     {
                         MessageBox.Show("Username telah dipakai", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
                         textBox_username.Focus();
+                    } else
+                    {
+                        //logik of nama
+                        if (string.IsNullOrEmpty(textBox_name.Text))
+                        {
+                            MessageBox.Show("Nama tidak boleh kosong", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            textBox_name.Focus();
+                        }
+                        else
+                        {
+                            // logic of tanggal_lahir
+                            // add .date for filter only date can go throught without time
+                            // and convert to string to filter time
+                            string dt = tanggal_lahir.Value.Date.ToString("yyyy-MM-dd");
+                            string dt_now = DateTime.Now.Date.ToString("yyyy-MM-dd");
+
+                            if (dt.Equals(dt_now))
+                            {
+                                MessageBox.Show("Masukkan tanggal lahir anda", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                tanggal_lahir.Focus();
+                                //Console.WriteLine(dt);
+                            }
+                            else
+                            {
+                                // logic of number telephone
+                                if (string.IsNullOrEmpty(textBox_noTelp.Text))
+                                {
+                                    MessageBox.Show("nomor telepon tidak boleh kosong", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                    textBox_noTelp.Focus();
+                                }
+                                else
+                                {
+                                    if(textBox_noTelp.Text.Length < 10)
+                                    {
+                                        MessageBox.Show("Masukkan nomor handphone anda dengan benar", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                        textBox_noTelp.Focus();
+                                    } else
+                                    {
+                                        //logic password
+                                        if (string.IsNullOrEmpty(textBox_password.Text))
+                                        {
+                                            MessageBox.Show("password tidak boleh kosong", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                            textBox_password.Focus();
+                                        }
+                                        else
+                                        {
+                                            if (textBox_password.Text.Length < 8)
+                                            {
+                                                MessageBox.Show("panjang password tidak boleh kurang dari 8 karakter", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                                textBox_password.Focus();
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
-                } else
+                    
+                }
+                else
                 {
-                    MessageBox.Show("Username tidak boleh kosong","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    MessageBox.Show("Username tidak boleh kosong", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     textBox_username.Focus();
-                }
-
-                //logik of nama
-                if(string.IsNullOrEmpty(textBox_name.Text))
-                {
-                    MessageBox.Show("Nama tidak boleh kosong", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    textBox_name.Focus();
-                }
-
-                // logic of tanggal_lahir
-                // add .date for filter only date can go throught without time
-                // and convert to string to filter time
-                string dt = tanggal_lahir.Value.Date.ToString("yyyy-M-d");
-                string dt_now = DateTime.Now.Date.ToString("yyyy-M-d");
-
-                if (dt.Equals(dt_now))
-                {
-                    MessageBox.Show("Masukkan tanggal lahir anda", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    tanggal_lahir.Focus();
                 }
             } catch
             {
