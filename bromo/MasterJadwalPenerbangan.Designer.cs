59d720afb7f02d66bb0a0f68c7e9012c57301f22
TUNBudi06 <anjaytun2006@gmail.com>
2024-02-18 20:25:52 +0700

    just insert and update data left for sql server

--- bromo/MasterJadwalPenerbangan.Designer.cs
+++ bromo/MasterJadwalPenerbangan.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.label_headline = new System.Windows.Forms.Label();
             this.label_body = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView_mjp = new System.Windows.Forms.DataGridView();
             this.label_kPenerbangan = new System.Windows.Forms.Label();
             this.label_dari = new System.Windows.Forms.Label();
             this.label_ke = new System.Windows.Forms.Label();
@@ -46,10 +46,11 @@
             this.picker_tanggal = new System.Windows.Forms.DateTimePicker();
             this.masked_waktu = new System.Windows.Forms.MaskedTextBox();
             this.masked_durasi = new System.Windows.Forms.MaskedTextBox();
-            this.domain_hargapertiket = new System.Windows.Forms.DomainUpDown();
             this.button_batal = new System.Windows.Forms.Button();
             this.button_simpan = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.numericUpDown_hargaPerTiket = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_mjp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_hargaPerTiket)).BeginInit();
             this.SuspendLayout();
             // 
             // label_headline
@@ -72,16 +73,17 @@
             this.label_body.TabIndex = 1;
             this.label_body.Text = "Semua jadwal penerbangan akan muncul disini";
             // 
-            // dataGridView1
+            // dataGridView_mjp
             // 
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.dataGridView_mjp.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(18, 90);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(775, 214);
-            this.dataGridView1.TabIndex = 2;
+            this.dataGridView_mjp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_mjp.Location = new System.Drawing.Point(18, 90);
+            this.dataGridView_mjp.Name = "dataGridView_mjp";
+            this.dataGridView_mjp.Size = new System.Drawing.Size(775, 214);
+            this.dataGridView_mjp.TabIndex = 2;
+            this.dataGridView_mjp.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_mjp_CellContentClick);
             // 
             // label_kPenerbangan
             // 
@@ -173,6 +175,7 @@
             // 
             // masked_kode
             // 
+            this.masked_kode.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.masked_kode.Location = new System.Drawing.Point(176, 346);
             this.masked_kode.Mask = "AA-0000";
             this.masked_kode.Name = "masked_kode";
@@ -181,6 +184,7 @@
             // 
             // combo_ke
             // 
+            this.combo_ke.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.combo_ke.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.combo_ke.FormattingEnabled = true;
             this.combo_ke.Location = new System.Drawing.Point(176, 411);
@@ -190,6 +194,8 @@
             // 
             // combo_dari
             // 
+            this.combo_dari.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.combo_dari.DisplayMember = "ID";
             this.combo_dari.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.combo_dari.FormattingEnabled = true;
             this.combo_dari.Location = new System.Drawing.Point(176, 379);
@@ -197,10 +203,10 @@
             this.combo_dari.Size = new System.Drawing.Size(183, 21);
             this.combo_dari.TabIndex = 13;
             this.combo_dari.ValueMember = "ID";
-            this.combo_dari.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // combo_maskapai
             // 
+            this.combo_maskapai.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.combo_maskapai.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.combo_maskapai.FormattingEnabled = true;
             this.combo_maskapai.Location = new System.Drawing.Point(176, 445);
@@ -210,14 +216,15 @@
             // 
             // picker_tanggal
             // 
+            this.picker_tanggal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.picker_tanggal.Location = new System.Drawing.Point(593, 346);
             this.picker_tanggal.Name = "picker_tanggal";
             this.picker_tanggal.Size = new System.Drawing.Size(200, 20);
             this.picker_tanggal.TabIndex = 17;
-            this.picker_tanggal.ValueChanged += new System.EventHandler(this.Tanggal_ValueChanged);
             // 
             // masked_waktu
             // 
+            this.masked_waktu.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.masked_waktu.Location = new System.Drawing.Point(593, 380);
             this.masked_waktu.Mask = "00:00";
             this.masked_waktu.Name = "masked_waktu";
@@ -227,32 +234,27 @@
             // 
             // masked_durasi
             // 
+            this.masked_durasi.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.masked_durasi.Location = new System.Drawing.Point(593, 411);
             this.masked_durasi.Mask = "00 J\\am 00 Menit";
             this.masked_durasi.Name = "masked_durasi";
             this.masked_durasi.Size = new System.Drawing.Size(200, 20);
             this.masked_durasi.TabIndex = 20;
             // 
-            // domain_hargapertiket
-            // 
-            this.domain_hargapertiket.Location = new System.Drawing.Point(593, 446);
-            this.domain_hargapertiket.Name = "domain_hargapertiket";
-            this.domain_hargapertiket.Size = new System.Drawing.Size(200, 20);
-            this.domain_hargapertiket.TabIndex = 21;
-            this.domain_hargapertiket.Text = "1";
-            this.domain_hargapertiket.SelectedItemChanged += new System.EventHandler(this.domainUpDown1_SelectedItemChanged);
-            // 
             // button_batal
             // 
+            this.button_batal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button_batal.Location = new System.Drawing.Point(593, 493);
             this.button_batal.Name = "button_batal";
             this.button_batal.Size = new System.Drawing.Size(95, 35);
             this.button_batal.TabIndex = 22;
             this.button_batal.Text = "Batal";
             this.button_batal.UseVisualStyleBackColor = true;
+            this.button_batal.Click += new System.EventHandler(this.button_batal_Click);
             // 
             // button_simpan
             // 
+            this.button_simpan.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button_simpan.Location = new System.Drawing.Point(698, 493);
             this.button_simpan.Name = "button_simpan";
             this.button_simpan.Size = new System.Drawing.Size(95, 35);
@@ -260,14 +262,43 @@
             this.button_simpan.Text = "Simpan";
             this.button_simpan.UseVisualStyleBackColor = true;
             // 
+            // numericUpDown_hargaPerTiket
+            // 
+            this.numericUpDown_hargaPerTiket.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown_hargaPerTiket.Increment = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown_hargaPerTiket.Location = new System.Drawing.Point(593, 446);
+            this.numericUpDown_hargaPerTiket.Maximum = new decimal(new int[] {
+            276447231,
+            23283,
+            0,
+            0});
+            this.numericUpDown_hargaPerTiket.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_hargaPerTiket.Name = "numericUpDown_hargaPerTiket";
+            this.numericUpDown_hargaPerTiket.Size = new System.Drawing.Size(198, 20);
+            this.numericUpDown_hargaPerTiket.TabIndex = 24;
+            this.numericUpDown_hargaPerTiket.ThousandsSeparator = true;
+            this.numericUpDown_hargaPerTiket.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
             // MasterJadwalPenerbangan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(811, 560);
+            this.Controls.Add(this.numericUpDown_hargaPerTiket);
             this.Controls.Add(this.button_simpan);
             this.Controls.Add(this.button_batal);
-            this.Controls.Add(this.domain_hargapertiket);
             this.Controls.Add(this.masked_durasi);
             this.Controls.Add(this.masked_waktu);
             this.Controls.Add(this.picker_tanggal);
@@ -283,14 +314,15 @@
             this.Controls.Add(this.label_ke);
             this.Controls.Add(this.label_dari);
             this.Controls.Add(this.label_kPenerbangan);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dataGridView_mjp);
             this.Controls.Add(this.label_body);
             this.Controls.Add(this.label_headline);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MasterJadwalPenerbangan";
             this.Text = "MasterJadwalPenerbangan";
             this.Load += new System.EventHandler(this.MasterJadwalPenerbangan_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_mjp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_hargaPerTiket)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -300,7 +332,7 @@
 
         private System.Windows.Forms.Label label_headline;
         private System.Windows.Forms.Label label_body;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView_mjp;
         private System.Windows.Forms.Label label_kPenerbangan;
         private System.Windows.Forms.Label label_dari;
         private System.Windows.Forms.Label label_ke;
@@ -316,8 +348,8 @@
         private System.Windows.Forms.DateTimePicker picker_tanggal;
         private System.Windows.Forms.MaskedTextBox masked_waktu;
         private System.Windows.Forms.MaskedTextBox masked_durasi;
-        private System.Windows.Forms.DomainUpDown domain_hargapertiket;
         private System.Windows.Forms.Button button_batal;
         private System.Windows.Forms.Button button_simpan;
+        private System.Windows.Forms.NumericUpDown numericUpDown_hargaPerTiket;
     }
 }
--- bromo/MasterJadwalPenerbangan.cs
+++ bromo/MasterJadwalPenerbangan.cs
@@ -18,6 +18,7 @@ namespace bromo
             InitializeComponent();
         }
         Utils conn = new Utils();
+        private bool updateData = false;
 
         public void loadDataBinding()
         {
@@ -34,17 +35,19 @@ namespace bromo
 
                         using (SqlDataAdapter sdaa = new SqlDataAdapter(cmd))
                         {
-                            DataSet dsBandara = new DataSet();
-                            sdaa.Fill(dsBandara, "Bandara");
+                            DataSet dsBK = new DataSet();
+                            DataSet dsBT = new DataSet();
+                            sdaa.Fill(dsBK, "Bandara");
+                            sdaa.Fill(dsBT, "Bandara");
 
-                            if (dsBandara.Tables["Bandara"].Rows.Count > 0)
+                            if (dsBK.Tables["Bandara"].Rows.Count > 0)
                             {
-                                combo_dari.DataSource = dsBandara.Tables["Bandara"];
+                                combo_dari.DataSource = dsBK.Tables["Bandara"];
                                 combo_dari.DisplayMember = "Nama";
                                 combo_dari.ValueMember = "ID";
                                 combo_dari.SelectedValue = 68;
 
-                                combo_ke.DataSource = dsBandara.Tables["Bandara"];
+                                combo_ke.DataSource = dsBT.Tables["Bandara"];
                                 combo_ke.DisplayMember = "Nama";
                                 combo_ke.ValueMember = "ID";
                                 combo_ke.SelectedValue = 68;
@@ -86,29 +89,119 @@ namespace bromo
             }
         }
 
-        public static void loadTable()
+        public void loadTable()
         {
+            using(SqlConnection sqls = conn.koneksi())
+            {
+                try
+                {
+                    sqls.Open();
+                    string query = "SELECT jp.ID AS ID, jp.KodePenerbangan as KodePenerbangan, bk.Nama AS BandaraKeberangkatan, bt.Nama AS BandaraTujuan, m.Nama AS Maskapai, jp.TanggalWaktuKeberangkatan AS TanggalWaktuKeberangkatan," +
+    " convert(nvarchar(2),jp.DurasiPenerbangan / 60) + ' Jam ' + convert(nvarchar(2),jp.DurasiPenerbangan % 60) + ' menit' AS DurasiPenerbangan," +
+    " jp.HargaPerTiket AS HargaPerTiket" +
+    " FROM JadwalPenerbangan AS jp" +
+    " JOIN Bandara AS bk ON jp.BandaraKeberangkatanID = bk.ID" +
+    " JOIN Bandara AS bt ON jp.BandaraTujuanID = bt.ID" +
+    " JOIN Maskapai AS m ON jp.MaskapaiID = m.ID order by TanggalWaktuKeberangkatan desc";
 
+
+                    SqlCommand sqlc = sqls.CreateCommand();
+                    sqlc.CommandText = query;
+                    sqlc.CommandType = CommandType.Text;
+
+                    SqlDataAdapter adapter = new SqlDataAdapter();
+                    adapter.SelectCommand = sqlc;
+
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    this.dataGridView_mjp.Rows.Clear();
+                    this.dataGridView_mjp.Columns.Clear();
+                    this.dataGridView_mjp.DataSource = dt;
+                    this.dataGridView_mjp.Columns["ID"].Visible = false;
+                    this.dataGridView_mjp.AllowUserToAddRows = false;
+                    Console.WriteLine(this.dataGridView_mjp.ColumnCount);
+
+                    DataGridViewButtonColumn btn_ubah = new DataGridViewButtonColumn();
+                    DataGridViewButtonColumn btn_hapus = new DataGridViewButtonColumn();
+                    btn_ubah.Name = "ubahData";
+                    btn_hapus.Name = "hapusData";
+                    btn_ubah.Text = "Ubah";
+                    btn_hapus.Text = "Hapus";
+                    btn_hapus.UseColumnTextForButtonValue = true;
+                    btn_ubah.UseColumnTextForButtonValue = true;
+                    this.dataGridView_mjp.Columns.Insert(8, btn_ubah);
+                    this.dataGridView_mjp.Columns.Insert(9, btn_hapus);
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine("error: " + ex);
+                }
+                finally
+                {
+                    sqls.Close();
+                }
+            }
         }
 
         private void MasterJadwalPenerbangan_Load(object sender, EventArgs e)
         {
             loadDataBinding();
+
+            loadTable();
         }
 
-        private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
-        {
 
+        private void dataGridView_mjp_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.ColumnIndex >= 0  && dataGridView_mjp.Columns[e.ColumnIndex].Name == "ubahData")
+            {
+                updateData = true;
+                DataGridViewRow row = dataGridView_mjp.Rows[e.RowIndex];
+                masked_kode.Text = row.Cells["KodePenerbangan"].Value.ToString();
+                combo_dari.Text = row.Cells["BandaraKeberangkatan"].Value.ToString();
+                combo_ke.Text = row.Cells["BandaraTujuan"].Value.ToString();
+                combo_maskapai.Text = row.Cells["Maskapai"].Value.ToString();
+
+                DateTime dt = DateTime.Parse(row.Cells["TanggalWaktuKeberangkatan"].Value.ToString());
+                picker_tanggal.Value = dt;
+                masked_waktu.Text = dt.ToString("HH:mm");
+                numericUpDown_hargaPerTiket.Value = int.Parse(row.Cells["HargaPerTiket"].Value.ToString());
+            }
+            if (e.ColumnIndex >= 0 && dataGridView_mjp.Columns[e.ColumnIndex].Name == "hapusData")
+            {
+                DataGridViewRow row = dataGridView_mjp.Rows[e.RowIndex];
+                using(SqlConnection sqls = conn.koneksi())
+                {
+                    try
+                    {
+                        sqls.Open();
+                        SqlCommand sqlc = sqls.CreateCommand();
+                        sqlc.CommandType = CommandType.Text;
+                        sqlc.CommandText = "delete from JadwalPenerbangan where ID = @id;";
+                        sqlc.Parameters.AddWithValue("@id", row.Cells["ID"].Value.ToString());
+                    } catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+
+                    } finally
+                    {
+                        sqls.Close();
+                    }
+                }
+            }
         }
 
-        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        private void button_batal_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void Tanggal_ValueChanged(object sender, EventArgs e)
-        {
-
+            updateData = false;
+            masked_kode.Clear();
+            combo_dari.SelectedValue = 68;
+            combo_ke.SelectedValue = 68;
+            combo_maskapai.SelectedText = "Adam Air";
+            picker_tanggal.Value = DateTime.Now;
+            masked_waktu.Clear();
+            numericUpDown_hargaPerTiket.Value = 1;
         }
     }
 }
